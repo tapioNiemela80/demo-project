994d5761db173cdc70066a9f0473358af431adf6
tapioNiemela80 <tapio_niemela1@yahoo.com>
2025-06-10 14:19:57 +0100

    more tests

--- project-demo/src/main/java/tn/demo/project/service/ProjectService.java
+++ project-demo/src/main/java/tn/demo/project/service/ProjectService.java
@@ -3,7 +3,6 @@ package tn.demo.project.service;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import tn.demo.common.DateService;
 import tn.demo.common.IDService;
 import tn.demo.project.controller.ContactPersonInput;
 import tn.demo.project.domain.*;
@@ -11,25 +10,24 @@ import tn.demo.project.events.TaskAddedToProjectEvent;
 import tn.demo.project.repository.ProjectRepository;
 
 import java.time.LocalDate;
-import java.util.UUID;
 
 @Service
 public class ProjectService {
     private final ProjectRepository projects;
-    private final DateService dateService;
     private final IDService IDService;
     private final ApplicationEventPublisher applicationEventPublisher;
+    private final ProjectFactory projectFactory;
 
-    public ProjectService(ProjectRepository projects, DateService dateService, tn.demo.common.IDService IDService, ApplicationEventPublisher applicationEventPublisher) {
+    public ProjectService(ProjectRepository projects, tn.demo.common.IDService IDService, ApplicationEventPublisher applicationEventPublisher, ProjectFactory projectFactory) {
         this.projects = projects;
-        this.dateService = dateService;
         this.IDService = IDService;
         this.applicationEventPublisher = applicationEventPublisher;
+        this.projectFactory = projectFactory;
     }
 
     @Transactional
     public ProjectId createProject(String name, String description, LocalDate estimatedEndDate, tn.demo.project.controller.TimeEstimation estimation, ContactPersonInput contactPerson) {
-        Project project = Project.createNew(IDService.newProjectId(), name, description, dateService.now(), estimatedEndDate, toEstimation(estimation), IDService.newContactPersonId(), contactPerson.name(), contactPerson.email());
+        Project project = projectFactory.createNew(name, description, estimatedEndDate, estimation, contactPerson);
         return new ProjectId(projects.save(project).getId());
     }
 
@@ -37,7 +35,7 @@ public class ProjectService {
     public ProjectTaskId addTaskTo(ProjectId projectId, String taskName, String description, tn.demo.project.controller.TimeEstimation estimation){
         ProjectTaskId taskId = IDService.newProjectTaskId();
         return projects.findById(projectId.value())
-                .map(project -> project.addTask(taskId, taskName, description, toEstimation(estimation)))
+                .map(project -> project.addTask(taskId, taskName, description, toDomain(estimation)))
                 .map(projects::save)
                 .map(project -> publishNewTaskAddedToProjectEvent(projectId, taskId))
                 .orElseThrow(() -> new UnknownProjectIdException(projectId.value()));
@@ -48,7 +46,7 @@ public class ProjectService {
         return taskId;
     }
 
-    private TimeEstimation toEstimation(tn.demo.project.controller.TimeEstimation estimation) {
+    private TimeEstimation toDomain(tn.demo.project.controller.TimeEstimation estimation) {
         return new TimeEstimation(estimation.hours(), estimation.minutes());
     }
 
--- project-demo/src/test/java/tn/demo/project/service/ProjectServiceTest.java
+++ project-demo/src/test/java/tn/demo/project/service/ProjectServiceTest.java
@@ -3,12 +3,9 @@ package tn.demo.project.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatcher;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.context.ApplicationEventPublisher;
-import tn.demo.common.DateService;
 import tn.demo.common.IDService;
 import tn.demo.project.controller.ContactPersonInput;
 import tn.demo.project.domain.*;
@@ -16,12 +13,11 @@ import tn.demo.project.events.TaskAddedToProjectEvent;
 import tn.demo.project.repository.ProjectRepository;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.argThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -29,32 +25,29 @@ class ProjectServiceTest {
     @Mock
     private ProjectRepository projects;
     @Mock
-    private DateService dateService;
-    @Mock
     private IDService IDService;
     @Mock
     private ApplicationEventPublisher applicationEventPublisher;
+    @Mock
+    private ProjectFactory projectFactory;
 
     private ProjectService underTest;
 
     @BeforeEach
     void setup(){
-        underTest = new ProjectService(projects, dateService, IDService, applicationEventPublisher);
+        underTest = new ProjectService(projects, IDService, applicationEventPublisher, projectFactory);
     }
 
     @Test
     void createsProject(){
         ProjectId id = new ProjectId(UUID.randomUUID());
         LocalDate endDate = LocalDate.of(2026,12,31);
-        LocalDateTime now = LocalDateTime.of(2026,6,1,18,0,0);
         tn.demo.project.controller.TimeEstimation timeEstimation = new tn.demo.project.controller.TimeEstimation(100,0);
-        when(IDService.newProjectId()).thenReturn(id);
-        when(dateService.now()).thenReturn(now);
-        Project mockProject = Mockito.mock(Project.class);
-        when(mockProject.getId()).thenReturn(id.value());
-        when(projects.save(argThat(newProjectWithId(id)))).thenReturn(mockProject);
-        ContactPersonId contactPersonId = new ContactPersonId(UUID.randomUUID());
-        when(IDService.newContactPersonId()).thenReturn(contactPersonId);
+        Project project = mock(Project.class);
+        when(projectFactory.createNew("test project", "test description", endDate, timeEstimation, new ContactPersonInput("name", "email"))).thenReturn(project);
+        when(project.getId()).thenReturn(id.value());
+        when(projects.save(project)).thenReturn(project);
+
         ProjectId actual = underTest.createProject( "test project", "test description", endDate, timeEstimation, new ContactPersonInput("name", "email"));
         assertEquals(id, actual);
     }
@@ -78,7 +71,7 @@ class ProjectServiceTest {
     }
 
     @Test
-    void throwsExceptionWhenUnknownProjectId(){
+    void throwsExceptionWhenUnknownProjectIdWhenTryingToAddTaskToProject(){
         ProjectId id = new ProjectId(UUID.randomUUID());
         String taskName = "taskName";
         String description = "task description";
@@ -87,10 +80,7 @@ class ProjectServiceTest {
         when(projects.findById(id.value())).thenReturn(Optional.empty());
 
         assertThrows(UnknownProjectIdException.class, () -> underTest.addTaskTo(id, taskName, description, new tn.demo.project.controller.TimeEstimation(0,5)));
-    }
-
-    private ArgumentMatcher<Project> newProjectWithId(ProjectId id) {
-        return project -> project.isNew() && project.hasId(id);
+        verifyNoInteractions(applicationEventPublisher);
     }
 
 }
