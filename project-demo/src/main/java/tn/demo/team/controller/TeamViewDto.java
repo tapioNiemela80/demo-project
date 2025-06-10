c35ebececce7d14ec826aae431f4be50930f9839
tapioNiemela80 <tapio_niemela1@yahoo.com>
2025-06-10 19:30:45 +0100

    TeamView impls

--- project-demo/src/main/java/tn/demo/team/controller/TeamViewDto.java
+++ project-demo/src/main/java/tn/demo/team/controller/TeamViewDto.java
@@ -4,4 +4,11 @@ import java.util.List;
 import java.util.UUID;
 
 public record TeamViewDto(UUID id, String name, List<TeamTaskViewDto> tasks, List<TeamMemberDto> members) {
+
+    public ActualSpentTime getActualWorkDone(){
+        return tasks.stream()
+                .map(TeamTaskViewDto::actualSpentTime)
+                .reduce(tn.demo.team.controller.ActualSpentTime.zero(), tn.demo.team.controller.ActualSpentTime::add);
+    }
+
 }
